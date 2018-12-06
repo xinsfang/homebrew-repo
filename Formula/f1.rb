3ab160dc02b5b1f535d765576a61ea9a72fa7ef5
Xinsheng Fang <xinshengfang@gmail.com>
2018-12-06 23:39:00 +0800

    update f1

--- Formula/f1.rb
+++ Formula/f1.rb
@@ -1,8 +1,8 @@
 class F1 < Formula
   desc ""
   homepage ""
-  url "https://github.com/xinsfang/f1/archive/v1.0.1.tar.gz"
-  sha256 "30111bdfbd2f0ab64d0f610e7421b5933dc379b8b388aaa47bbcc275c2006765"
+  url "https://github.com/xinsfang/f1/archive/v1.0.2.tar.gz"
+  sha256 "2edd1abd04d4bc0f8e4fa4b8bdb1aeee6ba69e87768f70d40cb4715a7697fd42"
 
   def install
     bin.install "f1"
