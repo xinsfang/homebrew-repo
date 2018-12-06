3f87c09efacad073ac2a4559191937e404cf18ed
Xinsheng Fang <xinshengfang@gmail.com>
2018-12-06 23:34:02 +0800

    Add f1

---
+++ Formula/f1.rb
@@ -0,0 +1,11 @@
+class F1 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/xinsfang/f1/archive/v1.0.1.tar.gz"
+  sha256 "30111bdfbd2f0ab64d0f610e7421b5933dc379b8b388aaa47bbcc275c2006765"
+
+  def install
+    bin.install "f1"
+  end
+
+end
